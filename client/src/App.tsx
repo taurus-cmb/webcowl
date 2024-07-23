fbe4939f5233695e7d80c56c7ef2f0f2b0fce870
Steven Benton <sbenton@princeton.edu>
2024-07-23 08:46:31 -0400

    rough site layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { HStack, VStack, SimpleGrid, Heading, Text } from '@chakra-ui/react'
+import { useQuery, useQueryClient, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <QueryClientProvider client={queryClient}>
+      <HStack spacing={4}>
+        <DataBox bg="tan" header="Test Data">
+          <Value label="Time" value="Mon May 25, 2025 10:25:31" />
+          <Value label="Frame" value={2191} />
+          <Value label="Noise" value={1.32} />
+          <Value label="Steppy" value={22.98} />
+        </DataBox>
+      </HStack>
+    </QueryClientProvider>
+  )
+}
+
+// FIXME types
+function DataBox({ bg, header, children }) {
+  // TODO maybe build around Table instead?
+  return (
+    <VStack bg={bg} p={4} spacing={1}>
+      <Heading as="h3" size="m">{header}</Heading>
+      <SimpleGrid columns={2} columnGap={4}>
+        {children}
+      </SimpleGrid>
+    </VStack>
+  )
+}
+
+// FIXME types
+function Value({ label, value }) {
+  //const queryClient = useQueryClient();
+  //const query = useQuery({queryKey: "test", queryFn=getTestData});
+  
+  return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Text>{label}</Text>
+      <Text>{value}</Text>
     </>
   )
 }
