1c0372ab717a09479169ac165dcd44a68465ab87
Steven Benton <sbenton@princeton.edu>
2024-07-26 10:04:10 -0400

    Change OWLBox to Card instead of basic VStack

--- client/src/components/OwlBox.tsx
+++ client/src/components/OwlBox.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { VStack, SimpleGrid, Heading } from '@chakra-ui/react'
+import { SimpleGrid, Heading, Card, CardBody,Center } from '@chakra-ui/react'
 
 export function OwlBox({ bg, header, children }: { bg: string, header: string, children?: React.ReactNode }) {
   // TODO maybe build around Table instead?
+  // TODO Use CardHeader for Heading? (would need to fix padding)
   return (
-    <VStack bg={bg} p={4} spacing={1}>
-      <Heading as="h3" size="m">{header}</Heading>
-      <SimpleGrid columns={2} columnGap={4}>
-        {children}
-      </SimpleGrid>
-    </VStack>
+    <Card bg={bg}>
+      <CardBody>
+        <Center><Heading as="h3" size="m">{header}</Heading></Center>
+        <SimpleGrid columns={2} columnGap={4}>
+          {children}
+        </SimpleGrid>
+      </CardBody>
+    </Card>
   )
 }
 
