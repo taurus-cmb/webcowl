b84ea0d46574d073663a500e10f08e24e91f29ec
Steven Benton <sbenton@princeton.edu>
2024-08-20 10:38:41 -0400

    disable router vite plugin
    this is for file-based routing, which I'm not using

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": "http://127.0.0.1:5000",
-    }
-  }
-})
+    },
+  },
+});
