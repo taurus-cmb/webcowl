64b04288389387363ee80f089dd57d51741f4e0c
Steven Benton <sbenton@princeton.edu>
2025-04-27 16:43:37 -0400

    actually use datastar, not updates

--- webcowl/owl/__init__.py
+++ webcowl/owl/__init__.py
@@ -1,4 +1,7 @@
 from quart import Blueprint, render_template
+from datastar_py.quart import ServerSentEventGenerator, make_datastar_response
+import asyncio
+from datetime import datetime
 
 __all__ = ["owl_bp"]
 
@@ -8,10 +11,17 @@ owl_bp = Blueprint("owl", __name__)
 async def main():
     return await render_template("owl/main.html")
 
-# @owl_bp.route('/latest', methods=["POST"])
-# async def latest_data():
-#     fields = request.json["fields"]
-#     if not app.config["FAKE_DATA"]:
-#         return read_latest_data(fields)
-#     else:
-#         return fake_latest_data(fields)
+@owl_bp.route('/updates')
+async def updates():
+    async def time_updates():
+        while True:
+            yield ServerSentEventGenerator.merge_signals({"field_time": f"{datetime.now().isoformat()}"})
+            await asyncio.sleep(1)
+
+    response = await make_datastar_response(time_updates())
+    return response
+    # fields = request.json["fields"]
+    # if not app.config["FAKE_DATA"]:
+    #     return read_latest_data(fields)
+    # else:
+    #     return fake_latest_data(fields)
