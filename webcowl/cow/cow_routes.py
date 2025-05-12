e8999a345b8c8498acb2c4405a5c449f60ab8e29
Steven Benton <sbenton@princeton.edu>
2025-05-12 15:31:31 -0400

    working value setting and validation

--- webcowl/cow/cow_routes.py
+++ webcowl/cow/cow_routes.py
@@ -1,4 +1,4 @@
-from quart import Blueprint, helpers, current_app, render_template, stream_with_context
+from quart import Blueprint, helpers, current_app, render_template, stream_with_context, request
 from datastar_py.quart import ServerSentEventGenerator, make_datastar_response
 import asyncio
 from pyface.proto_cmd import ProtoCmd, ProtoCmdError
@@ -38,7 +38,7 @@ async def _render_cow_command_view(app):
 
         return await make_datastar_response(sse_update())
 
-@cow_bp.route("/set_command/<path:cmd_path>", methods=["GET", "POST"])
+@cow_bp.route("/set_command/<path:cmd_path>", methods=["POST"])
 async def set_command(cmd_path):
     cmd_split = cmd_path.split("/")
     # remove empty segments from extra /
@@ -48,10 +48,44 @@ async def set_command(cmd_path):
 
 # extra route to handle empty (home) path
 # TODO maybe use the data of the post, rather than its route?
-@cow_bp.route("/set_command/", methods=["GET", "POST"])
+@cow_bp.route("/set_command/", methods=["POST"])
 async def set_command_home():
     return await set_command("")
 
+ERR_PREFIX = "_error_"
+
+@cow_bp.route("/update_field", methods=["POST"])
+# TODO could move most of this code to proto_cmd, if desired
+async def update_field():
+    json = await request.json
+    local_cmd = _get_protocmd()
+    remove = []
+    errors = {}
+    active_path = "/".join(local_cmd.active_path)
+    for signal, value in json.items():
+        path = signal.replace(local_cmd.signal_sep, "/")
+        errkey = ERR_PREFIX + signal
+        if not path.startswith(active_path):
+            remove.append(signal)
+        elif len(value) == 0:
+            # reset errors for empty fields
+            errors[errkey] = ""
+        else:
+            try:
+                # TODO make things consistent between lists and /
+                local_cmd.update_field(path.split("/"), value)
+                errors[errkey] = ""
+            except ProtoCmdError as e:
+                errors[errkey] = "ERROR: " + str(e)
+
+    async def command_signal_update():
+        yield ServerSentEventGenerator.remove_signals(remove)
+        yield ServerSentEventGenerator.merge_signals(errors)
+
+    return await make_datastar_response(command_signal_update())
+
+# route to load the commands when the page is first visited
+# TODO is it better to do this all as part of the initial page render?
 @cow_bp.route('/load')
 async def updates():
     return await _render_cow_command_view(current_app)
